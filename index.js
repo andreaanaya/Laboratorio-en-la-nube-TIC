7b3fd414dc696fa6b2e2e470a72b8ed411f62941
andreaanaya <andrea.anaya@alumnos.uneatlantico.es>
2025-04-28 09:43:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get(‘/’,(req,res)=> {
 });
 
 app.listen(port,()=> {
-   console.log(‘servidor escuchando en http://localhost:${port]’);
+   console.log(‘servidor escuchando en http://localhost:${port}’);
 });
